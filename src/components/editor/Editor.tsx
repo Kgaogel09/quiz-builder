37a7757430327c40662a494539aa52ac3790617d
Kgaogel09 <kgaogelothorn@gmail.com>
2025-11-20 23:18:20 +0200

    feat(Editor): enhance question editing with dynamic options and remove functionality

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -58,55 +58,74 @@ export default function Editor({
             onSaveQuiz();
           }}
         >
-          <div className="flex mb-3">
-            <div className="grow">
-              <div className="mb-4">
-                <h5 className="font-medium mb-1">Question 1</h5>
-                <input
-                  type="text"
-                  name="questionTitle"
-                  id="questionTitle"
-                  placeholder="Enter question title"
-                  onChange={() => {}}
-                  className="text-sm shadow-sm border-gray-100 bg-gray-100 block w-full rounded-md p-2"
-                />
-              </div>
-              <div>
-                <h5 className="font-medium">Options:</h5>
-                <div className="flex items-center mb-2 gap-2">
-                  <span className="mr-2">{String.fromCharCode(65 + 0)}.</span>
+          {questions.map((question, index) => (
+            <div className="flex mb-3">
+              <div className="grow">
+                <div className="mb-4">
+                  <h5 className="font-medium mb-1">Question {index + 1}</h5>
                   <input
                     type="text"
-                    name="questionOption"
-                    id="questionOption"
-                    value={"test"}
+                    name="questionTitle"
+                    id="questionTitle"
+                    placeholder="Enter question title"
                     onChange={() => {}}
                     className="text-sm shadow-sm border-gray-100 bg-gray-100 block w-full rounded-md p-2"
                   />
-                  <button className="inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-100 transition">
-                    <Check size={16} />
+                </div>
+                <div className="px-3">
+                  <h5 className="font-medium">Options:</h5>
+                  {question.options.map((option, optionIndex) => (
+                    <div className="flex items-center mb-2 gap-2">
+                      <span className="mr-2">
+                        {String.fromCharCode(65 + optionIndex)}.
+                      </span>
+                      <input
+                        type="text"
+                        name="questionOption"
+                        id="questionOption"
+                        value={""}
+                        onChange={() => {}}
+                        className="text-sm shadow-sm border-gray-100 bg-gray-100 block w-full rounded-md p-2"
+                        placeholder={`Option ${optionIndex + 1}`}
+                      />
+                      <button className="inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-100 transition">
+                        <Check size={16} />
+                      </button>
+                      <button className="inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-100 transition">
+                        <X size={16} />
+                      </button>
+                    </div>
+                  ))}
+                </div>
+                <div className="flex gap-4 mt-4">
+                  <button
+                    type="button"
+                    className="text-cyan-500 flex items-center gap-1  text-sm font-medium  border border-cyan-500 px-3 py-1 rounded-md shadow-sm"
+                  >
+                    Add Option
                   </button>
-                  <button className="inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-100 transition">
-                    <X size={16} />
+                  <button
+                    type="submit"
+                    className="flex items-center gap-1 text-red-500 text-sm font-medium border border-red-500 px-3 py-1 rounded-md shadow-sm"
+                  >
+                    Remove Question
                   </button>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
           <div className="flex gap-4 mt-4">
             <button
               type="button"
               className="flex items-center gap-1 text-white text-base font-medium bg-blue-600 border border-blue-600 px-3 py-1 rounded-md shadow-sm"
               onClick={addQuestion}
             >
-              <Plus size={20} />
               Add Question
             </button>
             <button
               type="submit"
               className="flex items-center gap-1 text-white text-base font-medium bg-green-600 border border-green-600 px-3 py-1 rounded-md shadow-sm"
             >
-              <Save size={20} />
               Save Quiz
             </button>
           </div>
