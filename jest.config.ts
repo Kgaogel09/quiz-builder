70facf442a071d14eefa930ef14261f3d0af1441
Kgaogel09 <kgaogelothorn@gmail.com>
2025-11-25 15:10:10 +0200

    config: enhance Jest configuration with ESM support and improved formatting

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,27 @@
-import type { Config } from 'jest'
-const config: Config = { testEnvironment: 'jest-environment-jsdom', transform: { '^.+\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }] }, moduleNameMapper: { '^.+\.(css|less|scss)$': 'identity-obj-proxy' }, setupFilesAfterEnv: ['<rootDir>/setupTests.ts'] }
-export default config
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+      useESM: true
+    }]
+  },
+  moduleNameMapper: {
+    '^.+\.(css|less|scss)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.(ts|tsx|js)',
+    '<rootDir>/src/**/?(*.)(spec|test).(ts|tsx|js)'
+  ]
+};
+
+export default config;
